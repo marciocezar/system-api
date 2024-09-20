95c6c7a7f3aab9853ead25cfcfea74af599909eb
Marcio Rogerio Cezar <marcio.cezar@etec.sp.gov.br>
2024-09-19 22:10:45 -0300

    Add files via upload

--- app.js
+++ app.js
@@ -2,10 +2,12 @@ const express = require('express');
 const app = express();
 const sequelize = require('./config/config');
 const userRoutes = require('./routes/userRoutes');
+const authRoutes = require('./routes/authRoutes');
 
 app.use(express.json());
 
 app.use('/api', userRoutes);
+app.use('/api', authRoutes);
 
 sequelize.sync()
     .then(() => {
--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -1,7 +1,8 @@
 const express = require('express');
 const router = express.Router();
 const userController = require('../controllers/userController');
+const authenticateToken = require('../middlewares/authMiddlewares');
 
-router.post('/users',userController.listUsers);
+router.get('/users',authenticateToken, userController.listUsers);
 
 module.exports = router;
